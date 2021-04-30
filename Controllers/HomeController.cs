8d96ef593e7ac79110d63afc3c25276a16308d74
Hitagi <include.ofstream.h@gmail.com>
2021-04-30 12:44:45 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ClaimsApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClaimsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Policy = "AgeLimit")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return Content("For all ages");
+        }
+    }
+}
---
+++ Infrastructure/AgeRequirement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ClaimsApp.Infrastructure
+{
+    public class AgeRequirement : IAuthorizationRequirement
+    {
+        protected internal int Age { get; set; }
+
+        public AgeRequirement(int age)
+        {
+            Age = age;
+        }
+    }
+}
